052cb7e5b462ee7a1b3f0e4162dc555ad2fd869b
Jiang Shunjia <ustcjsj@ustc.edu>
2023-06-11 21:49:57 +0800

    tutorial 1 answer checked

--- tutorial01/leptjson.c
+++ tutorial01/leptjson.c
@@ -24,8 +24,6 @@ static int lept_parse_null(lept_context* c, lept_value* v) {
   if (c->json[0] != 'u' || c->json[1] != 'l' || c->json[2] != 'l')
     return LEPT_PARSE_INVALID_VALUE;
   c->json += 3;
-  lept_parse_whitespace(&c);
-  if (*c->json != '\0') return LEPT_PARSE_ROOT_NOT_SINGULAR;
   v->type = LEPT_NULL;
   return LEPT_PARSE_OK;
 }
@@ -35,8 +33,6 @@ static int lept_parse_true(lept_context* c, lept_value* v) {
   if (c->json[0] != 'r' || c->json[1] != 'u' || c->json[2] != 'e')
     return LEPT_PARSE_INVALID_VALUE;
   c->json += 3;
-  lept_parse_whitespace(&c);
-  if (*c->json != '\0') return LEPT_PARSE_ROOT_NOT_SINGULAR;
   v->type = LEPT_TRUE;
   return LEPT_PARSE_OK;
 }
@@ -46,8 +42,6 @@ static int lept_parse_false(lept_context* c, lept_value* v) {
       c->json[3] != 'e')
     return LEPT_PARSE_INVALID_VALUE;
   c->json += 4;
-  lept_parse_whitespace(&c);
-  if (*c->json != '\0') return LEPT_PARSE_ROOT_NOT_SINGULAR;
   v->type = LEPT_FALSE;
   return LEPT_PARSE_OK;
 }
@@ -69,11 +63,16 @@ static int lept_parse_value(lept_context* c, lept_value* v) {
 
 int lept_parse(lept_value* v, const char* json) {
   lept_context c;
+  int ret;
   assert(v != NULL);
   c.json = json;
   v->type = LEPT_NULL;
   lept_parse_whitespace(&c);
-  return lept_parse_value(&c, v);
+  if ((ret = lept_parse_value(&c, v)) == LEPT_PARSE_OK) {
+    lept_parse_whitespace(&c);
+    if (*c.json != '\0') ret = LEPT_PARSE_ROOT_NOT_SINGULAR;
+  }
+  return ret;
 }
 
 lept_type lept_get_type(const lept_value* v) {
